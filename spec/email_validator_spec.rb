ae0d8a3ddee193fdaace2cf37a4e36595a5934c1
Brian Alexander <balexand@gmail.com>
2011-08-24 10:46:10 -0700

    revert back to simple, easy to read regex; closes #5

--- spec/email_validator_spec.rb
+++ spec/email_validator_spec.rb
@@ -20,14 +20,16 @@ describe EmailValidator do
 
   describe "validation" do
     context "given the valid emails" do
-      [ "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
+      [ "user@example.com",
+        " user@example.com ",
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
         "01234567890@numbers-in-local.net",
         "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
         "mixed-1234-in-{+^}-local@sld.net",
         "a@single-character-in-local.org",
         "\"quoted\"@sld.com",
         "\"\\e\\s\\c\\a\\p\\e\\d\"@sld.com",
-        "\"quoted-at-sign@sld.org\"@sld.com",
+        # "\"quoted-at-sign@sld.org\"@sld.com",
         "\"escaped\\\"quote\"@sld.com",
         "\"back\\slash\"@sld.com",
         "one-character-third-level@a.example.com",
@@ -41,9 +43,14 @@ describe EmailValidator do
         "country-code-tld@sld.uk",
         "country-code-tld@sld.rw",
         "local@sld.newTLD",
-        "punycode-numbers-in-tld@sld.xn--3e0b707e",
+        # "punycode-numbers-in-tld@sld.xn--3e0b707e",
         "local@sub.domains.com",
-        "bracketed-IP-instead-of-domain@[127.0.0.1]" ].each do |email|
+        # "bracketed-IP-instead-of-domain@[127.0.0.1]",
+        "aaa@bbb.co.jp",
+        "nigel.worthington@big.co.uk",
+        "f@c.com",
+        "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca"
+      ].each do |email|
 
         it "#{email.inspect} should be valid" do
           TestUser.new(:email => email).should be_valid
@@ -55,25 +62,28 @@ describe EmailValidator do
 
     context "given the invalid emails" do
       [ "",
+        "f@s",
+        "f@s.c",
+        "@bar.com",
         "test@example.com@example.com",
         "test@",
         "@missing-local.org",
         "! \#$%\`|@invalid-characters-in-local.org",
-        "(),:;\`|@more-invalid-characters-in-local.org",
+        # "(),:;\`|@more-invalid-characters-in-local.org",
         "<>@[]\`|@even-more-invalid-characters-in-local.org",
-        ".local-starts-with-dot@sld.com",
-        "local-ends-with-dot.@sld.com",
-        "two..consecutive-dots@sld.com",
-        "partially.\"quoted\"@sld.com",
-        "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
+        # ".local-starts-with-dot@sld.com",
+        # "local-ends-with-dot.@sld.com",
+        # "two..consecutive-dots@sld.com",
+        # "partially.\"quoted\"@sld.com",
+        # "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
         "missing-sld@.com",
-        "sld-starts-with-dashsh@-sld.com",
-        "sld-ends-with-dash@sld-.com",
+        # "sld-starts-with-dashsh@-sld.com",
+        # "sld-ends-with-dash@sld-.com",
         "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
         "missing-dot-before-tld@com",
         "missing-tld@sld.",
         " ",
-        "the-total-length@of-an-entire-address-cannot-be-longer-than-two-hundred-and-fifty-four-characters-and-this-address-is-255-characters-exactly-so-it-should-be-invalid-and-im-going-to-add-some-more-words-here-to-increase-the-lenght-blah-blah-blah-blah-blah-blah-blah-blah.org",
+        # "the-total-length@of-an-entire-address-cannot-be-longer-than-two-hundred-and-fifty-four-characters-and-this-address-is-255-characters-exactly-so-it-should-be-invalid-and-im-going-to-add-some-more-words-here-to-increase-the-lenght-blah-blah-blah-blah-blah-blah-blah-blah.org",
         "missing-at-sign.net",
         "unbracketed-IP@127.0.0.1",
         "invalid-ip@127.0.0.1.26",
