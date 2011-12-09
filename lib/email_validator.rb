d3c40e6d90468d83d48d9912d94f58aa9a0dacf5
Corin Langosch <info@corinlangosch.com>
2011-12-09 10:43:26 +0100

    fixed validation checks, introduced strict_mode

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -1,7 +1,9 @@
 # Based on work from http://thelucid.com/2010/01/08/sexy-validation-in-edge-rails-rails-3/
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    unless value =~ /^\s*([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*$/i
+    name_validation = %|a-z0-9+\\-._|
+    name_validation << %|&'*/=?^{}~| unless options[:strict_mode]
+    unless value =~ /^\s*([#{name_validation}]{1,64})@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*$/i
       record.errors.add(attribute, options[:message] || :invalid)
     end
   end
--- spec/email_validator_spec.rb
+++ spec/email_validator_spec.rb
@@ -4,6 +4,10 @@ class TestUser < TestModel
   validates :email, :email => true
 end
 
+class StrictUser < TestModel
+  validates :email, :email => {:strict_mode => true}
+end
+
 class TestUserAllowsNil < TestModel
   validates :email, :email => {:allow_nil => true}
 end
@@ -17,83 +21,146 @@ class TestUserWithMessage < TestModel
 end
 
 describe EmailValidator do
-
   describe "validation" do
-    context "given the valid emails" do
-      [ "user@example.com",
-        " user@example.com ",
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
-        "01234567890@numbers-in-local.net",
-        "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
-        "mixed-1234-in-{+^}-local@sld.net",
-        "a@single-character-in-local.org",
-        "\"quoted\"@sld.com",
-        "\"\\e\\s\\c\\a\\p\\e\\d\"@sld.com",
-        # "\"quoted-at-sign@sld.org\"@sld.com",
-        "\"escaped\\\"quote\"@sld.com",
-        "\"back\\slash\"@sld.com",
-        "one-character-third-level@a.example.com",
-        "single-character-in-sld@x.org",
-        "local@dash-in-sld.com",
-        "letters-in-sld@123.com",
-        "one-letter-sld@x.org",
-        "uncommon-tld@sld.museum",
-        "uncommon-tld@sld.travel",
-        "uncommon-tld@sld.mobi",
-        "country-code-tld@sld.uk",
-        "country-code-tld@sld.rw",
-        "local@sld.newTLD",
-        # "punycode-numbers-in-tld@sld.xn--3e0b707e",
-        "local@sub.domains.com",
-        # "bracketed-IP-instead-of-domain@[127.0.0.1]",
-        "aaa@bbb.co.jp",
-        "nigel.worthington@big.co.uk",
-        "f@c.com",
-        "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca"
-      ].each do |email|
-
-        it "#{email.inspect} should be valid" do
-          TestUser.new(:email => email).should be_valid
+    context "in normal mode" do
+      context "given the valid emails" do
+        [
+          "user@example.com",
+          " user@example.com ",
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
+          "01234567890@numbers-in-local.net",
+          "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
+          "mixed-1234-in-{+^}-local@sld.net",
+          "a@single-character-in-local.org",
+          "one-character-third-level@a.example.com",
+          "single-character-in-sld@x.org",
+          "local@dash-in-sld.com",
+          "letters-in-sld@123.com",
+          "one-letter-sld@x.org",
+          "uncommon-tld@sld.museum",
+          "uncommon-tld@sld.travel",
+          "uncommon-tld@sld.mobi",
+          "country-code-tld@sld.uk",
+          "country-code-tld@sld.rw",
+          "local@sld.newTLD",
+          # "punycode-numbers-in-tld@sld.xn--3e0b707e",
+          "local@sub.domains.com",
+          "aaa@bbb.co.jp",
+          "nigel.worthington@big.co.uk",
+          "f@c.com",
+          "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca",
+        ].each do |email|
+          it "#{email.inspect} should be valid" do
+            TestUser.new(:email => email).should be_valid
+          end
         end
-
       end
 
+      context "given the invalid emails" do
+        [
+          "",
+          " ",
+          "f@s",
+          "f@s.c",
+          "@bar.com",
+          "test@example.com@example.com",
+          "test@",
+          "@missing-local.org",
+          "! \#$%\`|@invalid-characters-in-local.org",
+          "<>@[]\`|@even-more-invalid-characters-in-local.org",
+          "partially.\"quoted\"@sld.com",
+          "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
+          "missing-sld@.com",
+#           "sld-starts-with-dashsh@-sld.com",
+#           "sld-ends-with-dash@sld-.com",
+          "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
+          "missing-dot-before-tld@com",
+          "missing-tld@sld.",
+          "missing-at-sign.net",
+          "unbracketed-IP@127.0.0.1",
+          "invalid-ip@127.0.0.1.26",
+          "another-invalid-ip@127.0.0.256",
+          "hans,peter@example.com",
+          "hans(peter@example.com",
+          "hans)peter@example.com",
+          "IP-and-port@127.0.0.1:25",
+        ].each do |email|
+          it "#{email.inspect} should not be valid" do
+            TestUser.new(:email => email).should_not be_valid
+          end
+        end
+      end
     end
 
-    context "given the invalid emails" do
-      [ "",
-        "f@s",
-        "f@s.c",
-        "@bar.com",
-        "test@example.com@example.com",
-        "test@",
-        "@missing-local.org",
-        "! \#$%\`|@invalid-characters-in-local.org",
-        # "(),:;\`|@more-invalid-characters-in-local.org",
-        "<>@[]\`|@even-more-invalid-characters-in-local.org",
-        # ".local-starts-with-dot@sld.com",
-        # "local-ends-with-dot.@sld.com",
-        # "two..consecutive-dots@sld.com",
-        # "partially.\"quoted\"@sld.com",
-        # "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
-        "missing-sld@.com",
-        # "sld-starts-with-dashsh@-sld.com",
-        # "sld-ends-with-dash@sld-.com",
-        "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
-        "missing-dot-before-tld@com",
-        "missing-tld@sld.",
-        " ",
-        # "the-total-length@of-an-entire-address-cannot-be-longer-than-two-hundred-and-fifty-four-characters-and-this-address-is-255-characters-exactly-so-it-should-be-invalid-and-im-going-to-add-some-more-words-here-to-increase-the-lenght-blah-blah-blah-blah-blah-blah-blah-blah.org",
-        "missing-at-sign.net",
-        "unbracketed-IP@127.0.0.1",
-        "invalid-ip@127.0.0.1.26",
-        "another-invalid-ip@127.0.0.256",
-        "IP-and-port@127.0.0.1:25" ].each do |email|
-
-        it "#{email.inspect} should not be valid" do
-          TestUser.new(:email => email).should_not be_valid
+    context "in strict mode" do
+      context "given the valid emails" do
+        [
+          "user@example.com",
+          " user@example.com ",
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
+          "01234567890@numbers-in-local.net",
+          "a@single-character-in-local.org",
+          "one-character-third-level@a.example.com",
+          "single-character-in-sld@x.org",
+          "local@dash-in-sld.com",
+          "letters-in-sld@123.com",
+          "one-letter-sld@x.org",
+          "uncommon-tld@sld.museum",
+          "uncommon-tld@sld.travel",
+          "uncommon-tld@sld.mobi",
+          "country-code-tld@sld.uk",
+          "country-code-tld@sld.rw",
+          "local@sld.newTLD",
+          # "punycode-numbers-in-tld@sld.xn--3e0b707e",
+          "local@sub.domains.com",
+          "aaa@bbb.co.jp",
+          "nigel.worthington@big.co.uk",
+          "f@c.com",
+          "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca",
+        ].each do |email|
+          it "#{email.inspect} should be valid" do
+            StrictUser.new(:email => email).should be_valid
+          end
         end
+      end
 
+      context "given the invalid emails" do
+        [
+          "",
+          " ",
+          "f@s",
+          "f@s.c",
+          "@bar.com",
+          "test@example.com@example.com",
+          "test@",
+          "@missing-local.org",
+          "! \#$%\`|@invalid-characters-in-local.org",
+          "<>@[]\`|@even-more-invalid-characters-in-local.org",
+          "partially.\"quoted\"@sld.com",
+          "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
+          "missing-sld@.com",
+#           "sld-starts-with-dashsh@-sld.com",
+#           "sld-ends-with-dash@sld-.com",
+          "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
+          "missing-dot-before-tld@com",
+          "missing-tld@sld.",
+          "missing-at-sign.net",
+          "unbracketed-IP@127.0.0.1",
+          "invalid-ip@127.0.0.1.26",
+          "another-invalid-ip@127.0.0.256",
+          "hans,peter@example.com",
+          "hans(peter@example.com",
+          "hans)peter@example.com",
+          "IP-and-port@127.0.0.1:25",
+
+          # additional entries for strict mode
+          "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
+          "mixed-1234-in-{+^}-local@sld.net",
+        ].each do |email|
+          it "#{email.inspect} should not be valid" do
+            StrictUser.new(:email => email).should_not be_valid
+          end
+        end
       end
     end
   end
