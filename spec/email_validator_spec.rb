0618b498cd3cf3971c674a6e0867dcbf02a7f0a0
Brian Alexander <balexand@gmail.com>
2011-12-09 21:59:44 -0700

    eliminate redundancy in specs by merging the 4 lists into 3: "valid emails", "invalid emails", and "technically valid but esoteric emails"

--- spec/email_validator_spec.rb
+++ spec/email_validator_spec.rb
@@ -21,146 +21,100 @@ class TestUserWithMessage < TestModel
 end
 
 describe EmailValidator do
+
   describe "validation" do
-    context "in normal mode" do
-      context "given the valid emails" do
-        [
-          "user@example.com",
-          " user@example.com ",
-          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
-          "01234567890@numbers-in-local.net",
-          "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
-          "mixed-1234-in-{+^}-local@sld.net",
-          "a@single-character-in-local.org",
-          "one-character-third-level@a.example.com",
-          "single-character-in-sld@x.org",
-          "local@dash-in-sld.com",
-          "letters-in-sld@123.com",
-          "one-letter-sld@x.org",
-          "uncommon-tld@sld.museum",
-          "uncommon-tld@sld.travel",
-          "uncommon-tld@sld.mobi",
-          "country-code-tld@sld.uk",
-          "country-code-tld@sld.rw",
-          "local@sld.newTLD",
-          # "punycode-numbers-in-tld@sld.xn--3e0b707e",
-          "local@sub.domains.com",
-          "aaa@bbb.co.jp",
-          "nigel.worthington@big.co.uk",
-          "f@c.com",
-          "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca",
-        ].each do |email|
-          it "#{email.inspect} should be valid" do
-            TestUser.new(:email => email).should be_valid
-          end
+    context "given the valid emails" do
+      [
+        "a+b@plus-in-local.com",
+        "a_b@underscore-in-local.com",
+        "user@example.com",
+        " user@example.com ",
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
+        "01234567890@numbers-in-local.net",
+        "a@single-character-in-local.org",
+        "one-character-third-level@a.example.com",
+        "single-character-in-sld@x.org",
+        "local@dash-in-sld.com",
+        "letters-in-sld@123.com",
+        "one-letter-sld@x.org",
+        "uncommon-tld@sld.museum",
+        "uncommon-tld@sld.travel",
+        "uncommon-tld@sld.mobi",
+        "country-code-tld@sld.uk",
+        "country-code-tld@sld.rw",
+        "local@sld.newTLD",
+        "local@sub.domains.com",
+        "aaa@bbb.co.jp",
+        "nigel.worthington@big.co.uk",
+        "f@c.com",
+        "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca"
+      ].each do |email|
+
+        it "#{email.inspect} should be valid" do
+          TestUser.new(:email => email).should be_valid
         end
+
+        it "#{email.inspect} should be valid in strict_mode" do
+          StrictUser.new(:email => email).should be_valid
+        end
+
       end
 
-      context "given the invalid emails" do
-        [
-          "",
-          " ",
-          "f@s",
-          "f@s.c",
-          "@bar.com",
-          "test@example.com@example.com",
-          "test@",
-          "@missing-local.org",
-          "! \#$%\`|@invalid-characters-in-local.org",
-          "<>@[]\`|@even-more-invalid-characters-in-local.org",
-          "partially.\"quoted\"@sld.com",
-          "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
-          "missing-sld@.com",
-#           "sld-starts-with-dashsh@-sld.com",
-#           "sld-ends-with-dash@sld-.com",
-          "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
-          "missing-dot-before-tld@com",
-          "missing-tld@sld.",
-          "missing-at-sign.net",
-          "unbracketed-IP@127.0.0.1",
-          "invalid-ip@127.0.0.1.26",
-          "another-invalid-ip@127.0.0.256",
-          "hans,peter@example.com",
-          "hans(peter@example.com",
-          "hans)peter@example.com",
-          "IP-and-port@127.0.0.1:25",
-        ].each do |email|
-          it "#{email.inspect} should not be valid" do
-            TestUser.new(:email => email).should_not be_valid
-          end
+    end
+
+    context "given the invalid emails" do
+      [
+        "",
+        "f@s",
+        "f@s.c",
+        "@bar.com",
+        "test@example.com@example.com",
+        "test@",
+        "@missing-local.org",
+        "! \#$%\`|@invalid-characters-in-local.org",
+        "<>@[]\`|@even-more-invalid-characters-in-local.org",
+        "missing-sld@.com",
+        "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
+        "missing-dot-before-tld@com",
+        "missing-tld@sld.",
+        " ",
+        "missing-at-sign.net",
+        "unbracketed-IP@127.0.0.1",
+        "invalid-ip@127.0.0.1.26",
+        "another-invalid-ip@127.0.0.256",
+        "IP-and-port@127.0.0.1:25",
+        "partially.\"quoted\"@sld.com",
+        "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
+        "hans,peter@example.com",
+        "hans(peter@example.com",
+        "hans)peter@example.com"
+      ].each do |email|
+
+        it "#{email.inspect} should not be valid" do
+          TestUser.new(:email => email).should_not be_valid
         end
+
+        it "#{email.inspect} should not be valid in strict_mode" do
+          StrictUser.new(:email => email).should_not be_valid
+        end
+
       end
     end
 
-    context "in strict mode" do
-      context "given the valid emails" do
-        [
-          "user@example.com",
-          " user@example.com ",
-          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ@letters-in-local.org",
-          "01234567890@numbers-in-local.net",
-          "a@single-character-in-local.org",
-          "one-character-third-level@a.example.com",
-          "single-character-in-sld@x.org",
-          "local@dash-in-sld.com",
-          "letters-in-sld@123.com",
-          "one-letter-sld@x.org",
-          "uncommon-tld@sld.museum",
-          "uncommon-tld@sld.travel",
-          "uncommon-tld@sld.mobi",
-          "country-code-tld@sld.uk",
-          "country-code-tld@sld.rw",
-          "local@sld.newTLD",
-          # "punycode-numbers-in-tld@sld.xn--3e0b707e",
-          "local@sub.domains.com",
-          "aaa@bbb.co.jp",
-          "nigel.worthington@big.co.uk",
-          "f@c.com",
-          "areallylongnameaasdfasdfasdfasdf@asdfasdfasdfasdfasdf.ab.cd.ef.gh.co.ca",
-        ].each do |email|
-          it "#{email.inspect} should be valid" do
-            StrictUser.new(:email => email).should be_valid
-          end
-        end
-      end
+    context "given the technically valid but esoteric emails" do
+      [
+        "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
+        "mixed-1234-in-{+^}-local@sld.net"
+      ].each do |email|
 
-      context "given the invalid emails" do
-        [
-          "",
-          " ",
-          "f@s",
-          "f@s.c",
-          "@bar.com",
-          "test@example.com@example.com",
-          "test@",
-          "@missing-local.org",
-          "! \#$%\`|@invalid-characters-in-local.org",
-          "<>@[]\`|@even-more-invalid-characters-in-local.org",
-          "partially.\"quoted\"@sld.com",
-          "the-local-part-is-invalid-if-it-is-longer-than-sixty-four-characters@sld.net",
-          "missing-sld@.com",
-#           "sld-starts-with-dashsh@-sld.com",
-#           "sld-ends-with-dash@sld-.com",
-          "invalid-characters-in-sld@! \"\#$%(),/;<>_[]\`|.org",
-          "missing-dot-before-tld@com",
-          "missing-tld@sld.",
-          "missing-at-sign.net",
-          "unbracketed-IP@127.0.0.1",
-          "invalid-ip@127.0.0.1.26",
-          "another-invalid-ip@127.0.0.256",
-          "hans,peter@example.com",
-          "hans(peter@example.com",
-          "hans)peter@example.com",
-          "IP-and-port@127.0.0.1:25",
-
-          # additional entries for strict mode
-          "&'*+-./=?^_{}~@other-valid-characters-in-local.net",
-          "mixed-1234-in-{+^}-local@sld.net",
-        ].each do |email|
-          it "#{email.inspect} should not be valid" do
-            StrictUser.new(:email => email).should_not be_valid
-          end
+        it "#{email.inspect} should be valid" do
+          TestUser.new(:email => email).should be_valid
         end
+
+        it "#{email.inspect} should not be valid in strict_mode" do
+          StrictUser.new(:email => email).should_not be_valid
+        end
+
       end
     end
   end
