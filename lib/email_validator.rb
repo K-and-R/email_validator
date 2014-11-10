b3b8fc239ff2435daab20098f6607eb1ca791658
PatrickGVice <patrickgvice@gmail.com>
2014-11-09 20:30:30 -0500

    Added a method for a new validation mode that checks the mail exchanger record for a domain of an email
    Added new dns_mode for email_validator
    
    Added tests for new dns validation mode
    
    Updated README to have information on new dns_mode

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -9,8 +9,18 @@ class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     options = @@default_options.merge(self.options)
     name_validation = options[:strict_mode] ? "-a-z0-9+._" : "^@\\s"
-    unless value =~ /\A\s*([#{name_validation}]{1,64})@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*\z/i
+    valid_dns = options[:dns_mode] ? (dns_lookup? value) : true
+    unless value =~ /\A\s*([#{name_validation}]{1,64})@((?:[-a-z0-9]+\.)+[a-z]{2,})\s*\z/i && valid_dns
       record.errors.add(attribute, options[:message] || :invalid)
     end
+  end
+
+  private
+  def dns_lookup? value
+    if value.match(/.*@(.*)/)
+      mx = Resolv::DNS.open.getresources $1, Resolv::DNS::Resource::IN::MX
+      return mx.size > 0 ? true : false
+    end
+    false
   end
 end
