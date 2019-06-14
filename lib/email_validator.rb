cb8e3b50e6d28c528dd233e700781dfbd20c3b21
Karl Wilbur <karl@kandrsoftware.com>
2019-06-14 11:32:39 -0400

    Refactor class methods for readability

--- lib/email_validator.rb
+++ lib/email_validator.rb
@@ -2,42 +2,63 @@
 class EmailValidator < ActiveModel::EachValidator
   @@default_options = {}
 
-  def self.default_options
-    @@default_options
-  end
+  class << self
+    def default_options
+      @@default_options
+    end
 
-  def self.regexp(options={})
+    def valid?(value, options={})
+      options = @@default_options.merge(options)
+      !!((value.present? || value.nil? && options[:allow_nil].present?) && value =~ regexp(options))
+    end
+
+    def invalid?(value, options={})
+      !valid?(value, options)
+    end
+
     # Refs:
     #  http://tools.ietf.org/html/rfc5321
     #  https://tools.ietf.org/html/rfc2822
+    def regexp(options={})
+      options = @@default_options.merge(options)
 
-    options = @@default_options.merge(options)
+      domain_pattern = options[:domain].sub(/\./, '\.') if options[:domain]
+      domain_pattern ||= "(?:#{label_pattern}\\.)*#{label_pattern}"
 
-    #Domain name matching
-    hyphen = '-'
-    alpha = "[[:alpha:]]"
-    alnum = "[[:alnum:]]"
-    alnumhy = "(?:#{alnum}|#{hyphen})"
-    label_pattern = "#{alpha}(?:#{alnumhy}{,62}#{alnum}+)?"
-    domain_pattern = options[:domain].sub(/\./, '\.') if options[:domain]
-    domain_pattern ||= "(?:#{label_pattern}\\.)*#{label_pattern}"
-    if options[:strict_mode]
-      # Local-part matching
-      atom_char = '[-\p{Cased_Letter}\p{Nd}+_!"\'#$%^&*{}/=?`\|~]'
-      local_part_pattern = "(?:#{atom_char}\\.?){,63}#{atom_char}"
-      regexp = /\A(?>#{local_part_pattern})@#{domain_pattern}\z/i
-    else
-      regexp = /\A\s*(?>[^@\s]{1,64})@#{domain_pattern}\s*\z/i
+      if options[:strict_mode]
+        # validate local part
+        regexp = /\A(?>#{local_part_pattern})@#{domain_pattern}\z/i
+      else
+        # no spaces or '@' in local part
+        regexp = /\A\s*(?>[^@\s]{1,64})@#{domain_pattern}\s*\z/i
+      end
     end
-  end
 
-  def self.valid?(value, options={})
-    options = @@default_options.merge(options)
-    !!((value.present? || value.nil? && options[:allow_nil].present?) && value =~ self.regexp(options))
-  end
+    protected
 
-  def self.invalid?(value, options={})
-    !valid?(value, options)
+    def alpha
+      "[[:alpha:]]"
+    end
+
+    def alnum
+      "[[:alnum:]]"
+    end
+
+    def alnumhy
+      "(?:#{alnum}|-)"
+    end
+
+    def label_pattern
+      "#{alpha}(?:#{alnumhy}{,62}#{alnum}+)?"
+    end
+
+    def atom_char
+      '[-\p{Cased_Letter}\p{Nd}+_!"\'#$%^&*{}/=?`\|~]'
+    end
+
+    def local_part_pattern
+      "(?:#{atom_char}\\.?){,63}#{atom_char}"
+    end
   end
 
   def validate_each(record, attribute, value)
